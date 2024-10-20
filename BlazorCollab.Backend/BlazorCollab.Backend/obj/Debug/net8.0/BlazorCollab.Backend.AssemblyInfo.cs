ca8725b5ee1441001d6a7f84527acdaab8efb75c
Adbspec <abhishekr0709@gmail.com>
2024-10-20 22:06:49 +0530

    .

--- BlazorCollab.Backend/BlazorCollab.Backend/obj/Debug/net8.0/BlazorCollab.Backend.AssemblyInfo.cs
+++ BlazorCollab.Backend/BlazorCollab.Backend/obj/Debug/net8.0/BlazorCollab.Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorCollab.Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1da9e4f0c4ff5b07a72dc8b14c349d51173fe5cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23ee429fb25af5ebb245bf913f172a5a97534a34")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorCollab.Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorCollab.Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
